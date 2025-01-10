881fadaea80122612dd262e80ea44d0548b409c3
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-01-09 17:02:40 -0800

    Add ExampleModal
    https://docs.obsidian.md/Plugins/User+interface/Modals#Accept+user+input

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Notice, Plugin } from "obsidian";
+import { App, Modal, Notice, Plugin, Setting } from "obsidian";
 
 // The following example reads the content of all Markdown files in the Vault and returns the average document size.
 export default class ExamplePlugin extends Plugin {
@@ -18,5 +18,36 @@ export default class ExamplePlugin extends Plugin {
     let totalLength = 0;
     fileContents.forEach((content) => totalLength += content.length);
     return totalLength / fileContents.length;
+  }
+}
+
+// TODO: Modal takes graph database connection URI by user input and
+// syncs the graph database with the Obsidian vault.
+// https://docs.obsidian.md/Plugins/User+interface/Modals#Accept+user+input
+
+export class ExampleModal extends Modal {
+  constructor(app: App, onSubmit: (result: string) => void) {
+    super(app);
+    this.setTitle("What's your name?");
+
+    let name = "";
+    new Setting(this.contentEl)
+      .setName("Name")
+      .addText((text) =>
+        text.onChange((value) => {
+          name = value;
+        })
+      );
+
+    new Setting(this.contentEl)
+      .addButton((btn) =>
+        btn
+          .setButtonText("Submit")
+          .setCta()
+          .onClick(() => {
+            this.close();
+            onSubmit(name);
+          })
+      );
   }
 }
